517fbd3f437b2f228faa1a4b68122ff4fe9edde3
Manoj Kumar <manoo2mail@gmail.com>
2015-03-24 18:13:06 +0530

    Done with Tail Recursion and Assignments

--- ScalaMaven/src/main/scala/learning/SampleExamples.scala
+++ ScalaMaven/src/main/scala/learning/SampleExamples.scala
@@ -12,6 +12,10 @@ object SampleExamples {
     println(perfectSquare(9))
   }
 
+  /**
+   * It returns an Integer which is a Sum Of N Numbers
+   *
+   */
   def sumOfNNumbers(n: Int): Int = {
     @annotation.tailrec
     def SumOfNNumbers(n: Int, acc: Int): Int = {
@@ -24,6 +28,9 @@ object SampleExamples {
     SumOfNNumbers(n, 0)
   }
 
+  /**
+   * It returns a Factorial of a given number
+   */
   def factorial(n: Int): Int = {
     @annotation.tailrec
     def Factorial(number: Int, acc: Int): Int = {
@@ -35,6 +42,9 @@ object SampleExamples {
     Factorial(n, 1)
   }
 
+  /**
+   * It returns Sum of factors of a Given Number
+   */
   def sumOfFactorsOfN(n: Int): Int = {
     @annotation.tailrec
     def SumOfFactorsOfN(n: Int, curr: Int, acc: Int): Int = {
@@ -50,6 +60,10 @@ object SampleExamples {
     SumOfFactorsOfN(n, 1, 0)
   }
 
+  /**
+   * It Returns True if a given number is Perfect Number
+   * else it returns false
+   */
   def perfectNumber(n: Int): Boolean = {
     @annotation.tailrec
     def SumOfFactorsOfN(n: Int, curr: Int, acc: Int): Int = {
@@ -65,6 +79,10 @@ object SampleExamples {
     (n == SumOfFactorsOfN(n, 1, 0))
   }
 
+  /**
+   * It Returns True if a given number is Perfect Square
+   * else Returns false
+   */
   def perfectSquare(n: Int): Boolean = {
     @annotation.tailrec
     def PerfectSquare(n: Int, acc: Int): Boolean = {
